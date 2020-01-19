2b59808da843b64fbafa88bec589f121c1f3dc00
wood503 <kwoodc503@gmail.com>
2020-01-19 13:45:41 -0500

    fixing exercise 9

--- src/Exercises.java
+++ src/Exercises.java
@@ -175,23 +175,23 @@ public class Exercises {
 
 	public boolean balance(int[] numbers) {
 		if(numbers == null) {
-						return false;
-					} else if (numbers.length < 2) {
-						return false;
-					}
-					for(int i = 0; i < numbers.length; i ++) {
-						int sumLeft = 0;
-						int sumRight = 0;
-						for(int j = 0; j < i; i ++) {
-							sumLeft += numbers[j];
-						}
-						for(int j = i; j < numbers.length; j ++) {
-							sumRight += numbers[j];
-						}
-						if(sumLeft == sumRight) {
-							return true;
-						}
-					}
+			return false;
+		} else if (numbers.length < 2) {
+			return false;
+		}
+		for(int i = 0; i < numbers.length; i ++) {
+			int sumLeft = 0;
+			int sumRight = 0;
+			for(int j = 0; j < i; j ++) {
+				sumLeft += numbers[j];
+			}
+			for(int j = i; j < numbers.length; j ++) {
+				sumRight += numbers[j];
+			}
+			if(sumLeft == sumRight) {
+				return true;
+			}
+		}
 		return false;	// default return value to ensure compilation
 	}
 
